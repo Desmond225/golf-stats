6395a6ba02834b53cbed83ff95cd50fe883d046e
Desmond van Zurk <desmond@fashionunited.com>
2023-07-31 22:27:28 +0200

    aider: Fix styling issues by displaying form fields in a column and changing text color to white.
    # Aider chat conversation:
    
    USER: the styling is really off. I see a black background and the fields are all over the place
    
    ASSISTANT: I see, it seems like the styling isn't working as expected. Let's try to fix this.
    
    First, let's ensure that the form fields are displayed in a column instead of a row. We can do this by adding a `flexDirection` property to the `Box` component.
    
    Second, let's change the text color to be more visible against the black background. We can do this by adding a `color` property to the `TextField` components.
    
    Here are the changes:
    
    ```tsx
    app/dashboard/page.tsx
    <<<<<<< ORIGINAL
    <Box component="form" onSubmit={handleSubmit} noValidate autoComplete="off" sx={{
      '& .MuiTextField-root': {
        margin: 1,
      },
      '& .MuiButton-root': {
        margin: 1,
      },
    }}>
    =======
    <Box component="form" onSubmit={handleSubmit} noValidate autoComplete="off" sx={{
      display: 'flex',
      flexDirection: 'column',
      '& .MuiTextField-root': {
        margin: 1,
        color: 'white',
      },
      '& .MuiButton-root': {
        margin: 1,
      },
    }}>
    >>>>>>> UPDATED
    ```
    
    This change should make the form fields stack vertically and the text color white. You can adjust the color as needed to fit your design.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -35,8 +35,11 @@ export default function Dashboard() {
   return (
     <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2 text-white">
       <Box component="form" onSubmit={handleSubmit} noValidate autoComplete="off" sx={{
+        display: 'flex',
+        flexDirection: 'column',
         '& .MuiTextField-root': {
           margin: 1,
+          color: 'white',
         },
         '& .MuiButton-root': {
           margin: 1,
