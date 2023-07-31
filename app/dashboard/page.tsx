f7d74df6480bb5ad414474f9026a31be7a8fa39b
Desmond van Zurk <desmond@fashionunited.com>
2023-07-31 21:37:23 +0200

    add dashboard page #1

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import Link from 'next/link'
+
+export default function Login() {
+  return (
+    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
+      dashboard
+    </div>
+  )
+}
